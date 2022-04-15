027515732f7b7e3f9789fed9a69b0121cad025b2
khanhhuy98 <97667322+khanhhuy98@users.noreply.github.com>
2022-04-15 09:10:49 +0700

    Update drv_vnet_stm32_lis3dsh.h

--- driver_vnet_lis3dsh/drv_vnet_stm32_lis3dsh.h
+++ driver_vnet_lis3dsh/drv_vnet_stm32_lis3dsh.h
@@ -9,30 +9,30 @@
 #include "string.h"
 #include "stdio.h"
 
-#define LIS3DSH_I2C_ADDR          0x3C
-#define LIS3DSH_WHO_AM_I_ADDR     0x0F
-#define SET_READ_SINGLE_CMD(x)		(x|0x80)
+#define LIS3DSH_I2C_ADDR       		0x3C 					//Dia chi LIS3dSH (slave address)
+#define LIS3DSH_WHO_AM_I_ADDR     	0x0F					//Dia chi thanh ghi Who_am_I
+#define SET_READ_SINGLE_CMD(x)		(x|0x80)			
 #define SET_WRITE_SINGLE_CMD(x)		(x&(~(0x80)))
-#define ADD_REG_OUT_X_L 					0x28
-#define ADD_REG_OUT_X_H						0x29
-#define ADD_REG_OUT_Y_L						0x2A
-#define ADD_REG_OUT_Y_H						0x2B
-#define ADD_REG_OUT_Z_L						0x2C
-#define ADD_REG_OUT_Z_H						0x2D
-#define CTRL_REG1     						0x21
-#define CTRL_REG3     						0x23
-#define CTRL_REG4    							0x20
-#define CTRL_REG5     						0x24
-#define CTRL_REG6     						0x25
-#define STATUS        						0x27
-#define THRS1_1       						0x57
-#define ST1_1         						0x40
-#define ST1_2        							0x41
-#define MASK1_B       						0x59
-#define MASK1_A      							0x5A
-#define SETT1         						0x5B
+#define ADD_REG_OUT_X_L 		0x28					//Dia chi thanh ghi byte low OUT_X
+#define ADD_REG_OUT_X_H			0x29					//Dia chi thanh ghi byte high OUT_X
+#define ADD_REG_OUT_Y_L			0x2A					//Dia chi thanh ghi byte low OUT_Y
+#define ADD_REG_OUT_Y_H			0x2B					//Dia chi thanh ghi byte high OUT_Y
+#define ADD_REG_OUT_Z_L			0x2C					//Dia chi thanh ghi byte low OUT_Z
+#define ADD_REG_OUT_Z_H			0x2D					//Dia chi thanh ghi byte high OUT_Z
+#define CTRL_REG1     			0x21
+#define CTRL_REG3     			0x23
+#define CTRL_REG4    			0x20
+#define CTRL_REG5     			0x24
+#define CTRL_REG6     			0x25
+#define STATUS        			0x27
+#define THRS1_1       			0x57
+#define ST1_1         			0x40
+#define ST1_2        			0x41
+#define MASK1_B       			0x59
+#define MASK1_A      			0x5A
+#define SETT1         			0x5B
 
-#define sensities 								500
+#define sensities 			500			
 
 
 typedef struct
