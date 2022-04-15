d6c6f2bf998dc05a4059c29406378f885bacc26e
khanhhuy98 <97667322+khanhhuy98@users.noreply.github.com>
2022-04-15 09:09:08 +0700

    Update drv_vnet_stm32_lis3dsh.c

--- driver_vnet_lis3dsh/drv_vnet_stm32_lis3dsh.c
+++ driver_vnet_lis3dsh/drv_vnet_stm32_lis3dsh.c
@@ -3,18 +3,18 @@ extern I2C_HandleTypeDef hi2c1;
 extern UART_HandleTypeDef huart2;
 
 
-void LIS3DSH_Init(motion *MotionData)
+void LIS3DSH_Init(motion *MotionData) // Init 
 {
 	MotionData->biasX = 0;
 	MotionData->biasY = 0;
 	MotionData->biasZ = 0;
 	MotionData->motionDectect = 0;
 }
-void LIS3DSH_Write(uint8_t reg, uint8_t value, uint8_t size)
+void LIS3DSH_Write(uint8_t reg, uint8_t value, uint8_t size) // ghi data vao thanh ghi reg
 {
-	HAL_I2C_Mem_Write(&hi2c1, LIS3DSH_I2C_ADDR, reg, 1, &value, size, 100);
+	HAL_I2C_Mem_Write(&hi2c1, LIS3DSH_I2C_ADDR, reg, 1, &value, size, 100); 
 }
-int LIS3DSH_Read(uint8_t reg, uint8_t size)
+int LIS3DSH_Read(uint8_t reg, uint8_t size) // doc 1 byte data tu thanh ghi reg
 {
 	int readval;
 	uint8_t pdata[2];
@@ -22,7 +22,7 @@ int LIS3DSH_Read(uint8_t reg, uint8_t size)
 	readval = pdata[0];
 	return readval;
 }
-void Config_Reg(uint8_t reg, uint8_t value)
+void Config_Reg(uint8_t reg, uint8_t value) // Config thanh ghi LIS3DSH
 {
 //	LIS3DSH_Write(CTRL_REG4, 0x4F, 1);
 //	LIS3DSH_Write(CTRL_REG6, 0x10, 1);
@@ -37,45 +37,43 @@ void Config_Reg(uint8_t reg, uint8_t value)
 //  LIS3DSH_Write(SETT1,0x01,1);
 	LIS3DSH_Write(reg, value, 1);
 }
-uint8_t Motion_Det(motion *MotionData)
+uint8_t Motion_Det(motion *MotionData) // Phat hien chuyen dong, gia tri tra = 0 khi khong chuyen dong va = 1 khi co chuyen dong
 {
 	int buff_L[3];
 	int buff_H[3];
 	
-	int x = 0;
 	
-	buff_L[0] = LIS3DSH_Read(ADD_REG_OUT_X_L,2);
-	buff_L[1] = LIS3DSH_Read(ADD_REG_OUT_Y_L,2);
-	buff_L[2] = LIS3DSH_Read(ADD_REG_OUT_Z_L,2);
-	buff_H[0] = LIS3DSH_Read(ADD_REG_OUT_X_H,2);
-	buff_H[1] = LIS3DSH_Read(ADD_REG_OUT_Y_H,2);
-	buff_H[2] = LIS3DSH_Read(ADD_REG_OUT_Z_H,2);
+	buff_L[0] = LIS3DSH_Read(ADD_REG_OUT_X_L,2); 	//Doc byte low tu thanh ghi truc X
+	buff_L[1] = LIS3DSH_Read(ADD_REG_OUT_Y_L,2);	//Doc byte low tu thanh ghi truc Y
+	buff_L[2] = LIS3DSH_Read(ADD_REG_OUT_Z_L,2);	//Doc byte low tu thanh ghi truc Z
+	buff_H[0] = LIS3DSH_Read(ADD_REG_OUT_X_H,2);	//Doc byte high tu thanh ghi truc X
+	buff_H[1] = LIS3DSH_Read(ADD_REG_OUT_Y_H,2);	//Doc byte high tu thanh ghi truc Y
+	buff_H[2] = LIS3DSH_Read(ADD_REG_OUT_Z_H,2);	//Doc byte high tu thanh ghi truc Z
 	
-	MotionData->accelX = INT16_C(buff_H[0]<<8|buff_L[0]);
-	MotionData->accelY = INT16_C(buff_H[1]<<8|buff_L[1]);
-	MotionData->accelZ = INT16_C(buff_H[2]<<8|buff_L[2]);
+	MotionData->accelX = INT16_C(buff_H[0]<<8|buff_L[0]);	//Tinh gia tri cua sensor truc X
+	MotionData->accelY = INT16_C(buff_H[1]<<8|buff_L[1]);	//Tinh gia tri cua sensor truc Y
+	MotionData->accelZ = INT16_C(buff_H[2]<<8|buff_L[2]);	//Tinh gia tri cua sensor truc Z
 	
-	MotionData->biasX = __fabs(MotionData->accelX - MotionData->prevX);
-	MotionData->biasY = __fabs(MotionData->accelY - MotionData->prevY);
-	MotionData->biasZ = __fabs(MotionData->accelZ - MotionData->prevZ);
+	MotionData->biasX = __fabs(MotionData->accelX - MotionData->prevX);	//Tinh Do sai lech gia tri giua 2 lan lay mau lien tiep cua truc X
+	MotionData->biasY = __fabs(MotionData->accelY - MotionData->prevY);	//Tinh Do sai lech gia tri giua 2 lan lay mau lien tiep cua truc Y
+	MotionData->biasZ = __fabs(MotionData->accelZ - MotionData->prevZ);	//Tinh Do sai lech gia tri giua 2 lan lay mau lien tiep cua truc Z
 	
-	if(((MotionData->biasX >= sensities)||(MotionData->biasY >= sensities)||(MotionData->biasZ >= sensities))&&((MotionData->prevX != 0)&&(MotionData->prevY != 0)&&(MotionData->prevZ != 0)))
+	//Ham so sanh Do lech gia tri giua 2 lan lay mau tra ve gia tri 0 hoac 1 
+	if(((MotionData->biasX >= sensities)||(MotionData->biasY >= sensities)||(MotionData->biasZ >= sensities))&&((MotionData->prevX != 0)&&(MotionData->prevY != 0)&&(MotionData->prevZ != 0)))  
 	{
 		MotionData->motionDectect = 1;
-		x =1;
 	}
 	else if((MotionData->biasX < sensities)&&(MotionData->biasY < sensities)&&(MotionData->biasZ < sensities))
 	{
 		MotionData->motionDectect = 0;
-		x = 0;
 	}
 	
-	MotionData->prevX = MotionData->accelX;
-	MotionData->prevY = MotionData->accelY;
-	MotionData->prevZ = MotionData->accelZ;
+	MotionData->prevX = MotionData->accelX;	//Gan prevX = gia tri lay mau truoc do
+	MotionData->prevY = MotionData->accelY;	//Gan prevY = gia tri lay mau truoc do
+	MotionData->prevZ = MotionData->accelZ;	//Gan prevZ = gia tri lay mau truoc do
 	
 
-	return x;
+	return MotionData->motionDectect;
 
 }
 
